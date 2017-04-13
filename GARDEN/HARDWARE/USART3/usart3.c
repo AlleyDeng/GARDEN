b01eca560c5f604ed2f90a82fbee4c8465c808ae
Chao <1300452844@qq.com>
2017-04-13 20:04:06 +0800

    透传开启

--- GARDEN/HARDWARE/USART3/usart3.c
+++ GARDEN/HARDWARE/USART3/usart3.c
@@ -3,19 +3,7 @@
 #include "stdarg.h"	 	 
 #include "stdio.h"	 	 
 #include "string.h"	 
-#include "timer.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//ALIENTEK STM32开发板
-//串口3驱动代码	   
-//正点原子@ALIENTEK
-//技术论坛:www.openedv.com
-//修改日期:2014/3/29
-//版本：V1.0
-//版权所有，盗版必究。
-//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
-//All rights reserved									  
-////////////////////////////////////////////////////////////////////////////////// 	   
+#include "timer.h" 
 
 //串口接收缓存区 	
 u8 USART3_RX_BUF[USART3_MAX_RECV_LEN]; 				//接收缓冲,最大USART3_MAX_RECV_LEN个字节.
@@ -70,15 +58,15 @@ void usart3_init(u32 bound)
 
  	USART_DeInit(USART3);  //复位串口3
 		 //USART3_TX   PB10
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; //PB10
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//复用推挽输出
-  GPIO_Init(GPIOB, &GPIO_InitStructure); //初始化PB10
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; //PB10
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//复用推挽输出
+	GPIO_Init(GPIOB, &GPIO_InitStructure); //初始化PB10
    
     //USART3_RX	  PB11
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//浮空输入
-  GPIO_Init(GPIOB, &GPIO_InitStructure);  //初始化PB11
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//浮空输入
+	GPIO_Init(GPIOB, &GPIO_InitStructure);  //初始化PB11
 	
 	USART_InitStructure.USART_BaudRate = bound;//波特率一般设置为9600;
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
@@ -93,7 +81,7 @@ void usart3_init(u32 bound)
 	USART_Cmd(USART3, ENABLE);                    //使能串口 
 	
 	//使能接收中断
-  USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);//开启中断   
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);//开启中断   
 	
 	//设置中断优先级
 	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
@@ -121,7 +109,7 @@ void u3_printf(char* fmt,...)
 	i=strlen((const char*)USART3_TX_BUF);		//此次发送数据的长度
 	for(j=0;j<i;j++)							//循环发送数据
 	{
-	  while(USART_GetFlagStatus(USART3,USART_FLAG_TC)==RESET); //循环发送,直到发送完毕   
+		while(USART_GetFlagStatus(USART3,USART_FLAG_TC)==RESET); //循环发送,直到发送完毕   
 		USART_SendData(USART3,USART3_TX_BUF[j]); 
 	} 
 }
--- GARDEN/USER/main.c
+++ GARDEN/USER/main.c
@@ -2,21 +2,22 @@
 #include "delay.h"
 #include "usart.h" 
 #include "led.h" 		 	  
-#include "key.h"     	
+#include "key.h"
+#include "adc.h"
 #include "usart3.h"
 #include "common.h" 
 #include "string.h"
 #include "stdint.h"
-#include "ds18b20.h"
 
 uint8_t LED_Flag = 0;
 uint8_t sendDataFlag = 0;
-float temperature = 0;
 uint8_t str[200];
 
 int main(void)
 {	 
-	uint8_t key;
+	float temp = 0;
+	uint8_t key = 0;
+	uint16_t adcx = 0;
 	
 	delay_init();	    	 	//延时函数初始化	  
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
@@ -37,21 +38,13 @@ int main(void)
 	while (atk_8266_send_cmd("AT+CIPSTART=\"TCP\",\"192.168.199.101\",8086", "OK", 20));	/* 连接到服务器 */
 //	while (atk_8266_send_cmd("AT+CIPSTART=\"TCP\",\"119.29.80.76\",8086", "OK", 20));
 	atk_8266_at_response(1);
-	
-	while(DS18B20_Init())	//DS18B20初始化	
-	{
-		LCD_ShowString(30,130,200,16,16,"DS18B20 Error");
-		delay_ms(200);
-		LCD_Fill(30,130,239,130+16,WHITE);
- 		delay_ms(200);
-	}	
+	while (atk_8266_send_cmd("AT+CIPMODE=1", "OK", 20));				/* 开启透传模式 */
+	atk_8266_at_response(1);
+	while (atk_8266_send_cmd("AT+CIPSEND", "OK", 20));				/* 开始透传 */
+	atk_8266_at_response(1);
 	
 	while (1) {
 		GPIO_ResetBits(GPIOB, GPIO_Pin_5);
-		
-		temperature = DS18B20_Get_Temp() / 10;
-		sprintf((char *)str, "temperature:%3.1fC", temperature);
-		LCD_ShowString(30, 130, 200, 16, 16, str);
 		
 		key = KEY_Scan(0);
 		if (key == 4) {
@@ -59,18 +52,22 @@ int main(void)
 		}
 		if (sendDataFlag == 1) {
 			sendDataFlag = 0;
-			while (atk_8266_send_cmd("AT+CIPSEND=17", "OK", 20));	
-			atk_8266_at_response(1);
-			while (atk_8266_send_data(str, "OK", 20));
-			atk_8266_at_response(1);
+			u3_printf("Hello\n");
 		}
-		if (key == 1) {
-			while (atk_8266_send_cmd("AT+CIPSEND=5", "OK", 20));	
-			atk_8266_at_response(1);
-			while (atk_8266_send_data("World", "OK", 20));
-			atk_8266_at_response(1);
-		}
+		
+		adcx = Get_Adc_Average(ADC_Channel_1, 10);
+        sprintf((char *)str, "ADC_CH0_VAL:%d", adcx);
+        LCD_ShowString(60, 130, 200, 16, 16, str);	/* 显示ADC的值 */
+
+        temp = (float)adcx * (3.3 / 4096);
+        adcx = temp;
+        sprintf((char *)str, "ADC_CH0_VOL:%.3fV", temp);
+        LCD_ShowString(60, 150, 200, 16, 16, str);	/* 显示电压值 */
+
+		LCD_ShowString(20, 150, 200, 16, 16, "ADC:");
 		atk_8266_at_response(1);
+
+		
 		if (LED_Flag == 1) {
 			GPIO_ResetBits(GPIOE, GPIO_Pin_5);
 		} else if (LED_Flag == 0) {
